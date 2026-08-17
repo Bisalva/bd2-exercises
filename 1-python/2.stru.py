b4fb5116fef175aaec7e85154d2a939baa1de4cc
Jacquelineald <jacqueline.aaldridge@gmail.com>
2026-08-17 14:59:14 -0300

    update script

--- 1-python/2.stru.py
+++ 1-python/2.stru.py
@@ -1,3 +1,5 @@
+# Listas 
+
 numeros = [1,2,3,4,5, "uno","dos","tres","cuatro"]
 
 for numero in numeros:
@@ -67,3 +69,82 @@ for num in nums:
         cuadrados.append(num**2)
     
 cuadrados_impares = [num**2 for num in nums if num%2 ==1]
+
+# Diccionarios
+persona = {"nombre": "Camila", "edad": 25, "ciudad":"Punta Arenas",
+           130: True, "hijos":{"nombre":"Juan", "edad":12}
+           }
+print(persona)
+print(persona["nombre"])
+#print(persona["Pais"]) #Error
+print(persona.get("Pais"))
+print(persona.get("Pais","Chile"))
+print(f"Nombre: {persona['nombre']}\nEdad: {persona['edad']}\nciudad: {persona['ciudad']}")
+print(f"Nombre {persona['nombre']}, hijos {persona['hijos']}")
+
+persona_con_hijos = {"nombre":"Juan",
+                     "hijos":[{"nombre": "José"}, {"nombre":"Camilo"}]}
+persona_sin_hijos = {"nombre": "Alejandra"}
+
+print(f"Nombre: {persona_con_hijos['nombre']}, Hijos: {len(persona_con_hijos['hijos'])}")
+
+print(len(persona_sin_hijos.get("hijos",[])))
+print(len(persona_con_hijos.get("hijos",[])))
+if len(persona_sin_hijos.get("hijos",[])) == 0 :
+    print(f"Nombre {persona_sin_hijos['nombre']}, Hijos: No")
+else:
+    print(f"Nombre {persona_sin_hijos['nombre']}, Hijos: Si")
+    
+# Operador ternario
+# valor_true if condicion else valor_false
+print(f"Hijos: {'Si 'if len(persona_sin_hijos.get('hijos',[])) > 0 else 'No'}")
+print(persona)
+del persona[130]
+persona.pop("hijos")
+print(persona)
+
+print(f"Claves del diccionario: {persona.keys()}")
+print(f"Valores del diccionario: {persona.values()}")
+print(f"Clave-valor: {persona.items()}")
+
+for clave in persona.keys():
+    print(clave, persona[clave])
+    
+for clave, valor in persona.items():
+    print(f"{clave}={valor}")
+    
+
+# Ejercicios
+## 3. Recorre la lista de diccionarios y, para cada estudiante, determina si aprobó o reprobó
+## considerando que se aprueba con una nota mayor o igual a 4.0. 
+## Además, calcula el promedio del curso y cuenta cuántos estudiantes aprobaron. 
+## Imprime por cada estudiante un mensaje usando un f-string indicando su nombre, su nota y si aprobó o reprobó.
+
+estudiantes = [
+    {"nombre": "Ana", "nota": 5.5},
+    {"nombre": "Pedro", "nota": 3.2},
+    {"nombre": "Maria", "nota": 6.8},
+    {"nombre": "Juan", "nota": 3.9},
+    {"nombre": "Sofia", "nota": 4.0},
+]
+
+cantidad_estudiantes_aprobados = 0
+suma_notas = 0
+for estudiante in estudiantes:
+    # print(f"Estudiante: {estudiante}")
+    suma_notas = suma_notas + estudiante["nota"]
+    if(estudiante["nota"]>=4.0):
+        print(f"Nombre: {estudiante['nombre']}, nota {estudiante['nota']} (Aprobado)")
+        cantidad_estudiantes_aprobados = cantidad_estudiantes_aprobados + 1
+    else:
+        print(f"Nombre: {estudiante['nombre']}, nota {estudiante['nota']} (Reprobado)")
+promedio = suma_notas / len(estudiantes)
+print(f"Cantidad de aprobados: {cantidad_estudiantes_aprobados}\nPromedio:{promedio}")
+
+
+## 4. Implementa el ejercicio anterior utilizando comprensión de listas.
+
+## 5. Utilizando la variable mensaje, crea un diccionario que almacene la cantidad de veces que aparece cada palabra. 
+## Luego, imprime cada palabra junto con su número de apariciones. 
+## Antes de comenzar, asegúrate de convertir todas las letras del mensaje a minúsculas.
+texto = "Python es genial ! Python es fácil de aprender y Python es muy usado"
