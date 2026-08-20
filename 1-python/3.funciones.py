b07f801a6af40b8628a6c468a6481f3c90d5ac85
Jacquelineald <jacqueline.aaldridge@gmail.com>
2026-08-20 17:35:28 -0300

    add exercises: 6 and 7

--- 1-python/3.funciones.py
+++ 1-python/3.funciones.py
@@ -53,9 +53,39 @@ except Exception as e:
     print(f"ocurrio un error {num1=} {num2=} ({e})")
     
 
+# Ejercicios
+## 6. Crea una función crear_perfil(nombre, edad, altura, es_estudiante) 
+## que reciba los datos de una persona y retorne un string formateado
+## con toda la información usando f-strings.
+def crear_perfil(nombre, edad, altura, es_estudiante):
+                        #valor_True condicion valor_False
+    mensaje_estudiante = f"{'Si' if es_estudiante else 'No'}"
+    if es_estudiante:
+        print("Si")
+        mensaje_estudiante = "Si"
+    else:
+        mensaje_estudiante = "No"
+        
+    print(mensaje_estudiante)
+    return f"Nombre: {nombre}\nEdad: {edad}\n Altura: {altura}\n Estudiante: {mensaje_estudiante}"
 
+print(crear_perfil("Juan",25,1.60, False))
 
+## 7. Crea una función crear_perfil_dict(persona) que reciba un diccionario 
+## con los datos de una persona y retorne un string formateado con toda
+## la información usando f-strings. 
+## El diccionario debe contener las siguientes claves: nombre,edad,altura. 
+## La clave es_estudiante es un valor booleano opcional, por lo que se debe verificar si existe, 
+## en caso de no existir deben indicar que no es estudiante.
 
+def crear_perfil_dict(persona):
+    nombre = persona.get("nombre") #persona["nombre"]
+    edad = persona.get("edad")
+    altura = persona.get("altura")
+    es_estudiante = persona.get("es_estudiante",False) 
+    mensaje_estudiante = f"{'Si' if es_estudiante else 'No'}"
+    
+    return f"Nombre: {nombre}\nEdad: {edad}\nAltura: {altura}\nEs estudiante:{mensaje_estudiante}"
 
-
-
+persona = {"nombre":"Camila", "edad":23, "altura":1.77, "es_estudiante":False}
+print(crear_perfil_dict(persona))
