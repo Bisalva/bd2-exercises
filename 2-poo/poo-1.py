fd06bc96b6331aa374809aae343e590b81fde8bf
Jacquelineald <jacqueline.aaldridge@gmail.com>
2026-08-27 15:28:13 -0300

    add POO scr

--- 2-poo/poo-1.py
+++ 2-poo/poo-1.py
@@ -25,10 +25,24 @@ class Estudiante(Persona):
     def __init__(self, nombre,edad, carrera, NOTAS):
         super().__init__(nombre,edad)
         self.carrera = carrera
-        self.notas = NOTAS
-        
+        #self.notas = NOTAS # atributo publico
+        #self._notas = NOTAS # atributo protegido
+        self.__notas = NOTAS # atributo privado
+    
+    def __str__(self):
+        return f"{self.nombre} --> {self.__notas} "
+    
     def calcular_promedio(self):
-        return sum(self.notas) / len(self.notas)
+        return sum(self.__notas) / len(self.__notas)
+    
+    def presentarse(self):
+        return f"Hola soy {self.nombre}, soy estudiante de la carrera de {self.carrera} y tengo {self.edad}"
+    
+    def agregar_nota(self, nota):
+        self.__notas.append(nota)
+
+    def vaciar_notas(self):
+        self.__notas.clear()
         
 estudiante_1 = Estudiante("Ana", 22, "Ing. en computación", [5,6.2,4.3, 5.5])
 print(estudiante_1)      
@@ -44,6 +58,8 @@ class Profesor(Persona):
     def describir(self):
         return f"Soy {self.nombre} y soy profesor de {self.departamento}"
     
+    def presentarse(self):
+        return f"Hola, soy {self.nombre}, y soy profesor del departamento de {self.departamento}"
 profesor_1 = Profesor("Javier Pérez", 45, "Departamento de Computación", 2500000)
 
 print(profesor_1)
@@ -60,7 +76,28 @@ class EstudianteDeportista(Estudiante, Deportista):
     def __init__(self, nombre, edad, carrera, notas, deporte):
         Estudiante.__init__(self, nombre, edad,carrera, notas)
         Deportista.__init__(self,deporte)
+    
+    def presentarse(self):
+        return f"Hola soy {self.nombre}, estudio {self.carrera} y entreno {self.deporte}"
+        
+        
         
 estudiante_dep_1 = EstudianteDeportista("María José", 27, "Ing. en contrucción", 
                                         [6.5], "Atletismo")
 print(estudiante_dep_1.entrenar())
+print(f"{'-'*30}")
+print(estudiante_1.presentarse())
+print(profesor_1.presentarse())
+print(estudiante_dep_1.presentarse())
+
+print(f"{'-'*50}")
+estudiante_2 = Estudiante("Carlos", 25, "Ing. en electricidad", [5,6.2,4.3, 5.5])
+print(estudiante_2.presentarse())
+print(estudiante_2.calcular_promedio())
+# estudiante_2.__notas = [7.0] # No se debe hacer nunca porque se rompe el encapsulamiento
+print(estudiante_2.calcular_promedio())
+print(estudiante_2.__dict__)
+estudiante_2.agregar_nota(7.0)
+print(estudiante_2.__dict__)
+estudiante_2.vaciar_notas()
+print(estudiante_2.__dict__)
